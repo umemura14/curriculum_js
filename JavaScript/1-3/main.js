77f4f42b108b40c8d5cd4d6a987592d16fc4ee13
umemura14 <77683391+umemura14@users.noreply.github.com>
2021-07-08 19:55:20 +0900

    課題提出
    課題提出

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,25 @@
+//問１
+let a = 5;
+let b = 10;
+
+console.log(a != 10);
+console.log(b >= 10 || b < 20 || b % 2 === 0);
+
+//問２
+let x = 10;
+
+if (x >= 10 || x <= 20) {
+    console.log("成功です");
+}else {
+    console.log("失敗です");
+}
+
+//問３
+let y = 0;
+
+if(y % 2 === 0) {
+    console.log("偶数です")
+}
+if(y % 3 === 0) {
+    console.log("奇数です");
+}
